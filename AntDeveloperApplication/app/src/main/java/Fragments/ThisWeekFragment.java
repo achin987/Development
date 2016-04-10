cd11c20ef4afdbc5a5a6bf00d3b831d8a27cf9ed
achin987 <achin987@gmail.com>
2016-04-11 02:21:30 +0530

    Ant Development Project
    Base of the Project

---
+++ AntDeveloperApplication/app/src/main/java/Fragments/ThisWeekFragment.java
@@ -0,0 +1,30 @@
+package Fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import application.first.ant.antdeveloperapplication.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ThisWeekFragment extends Fragment {
+
+
+    public ThisWeekFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_this_week, container, false);
+    }
+
+}
---
+++ AntDeveloperApplication/app/src/main/java/application/first/ant/antdeveloperapplication/TabFragment.java
@@ -0,0 +1,108 @@
+package application.first.ant.antdeveloperapplication;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import Fragments.EventsFragment;
+import Fragments.FavouritesFragment;
+import Fragments.NotificationsFragment;
+import Fragments.ThisWeekFragment;
+
+public class TabFragment extends Fragment {
+
+    public static TabLayout tabLayout;
+    public static ViewPager viewPager;
+    public static int int_items = 4 ;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        /**
+         *Inflate tab_layout and setup Views.
+         */
+        View x =  inflater.inflate(R.layout.tab_layout,null);
+        tabLayout = (TabLayout) x.findViewById(R.id.tabs);
+        viewPager = (ViewPager) x.findViewById(R.id.viewpager);
+
+
+        /**
+         *Set an Apater for the View Pager
+         */
+        viewPager.setAdapter(new MyAdapter(getChildFragmentManager()));
+
+        /**
+         * Now , this is a workaround ,
+         * The setupWithViewPager dose't works without the runnable .
+         * Maybe a Support Library Bug .
+         */
+
+        tabLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                tabLayout.setupWithViewPager(viewPager);
+            }
+        });
+
+        return x;
+
+    }
+
+    class MyAdapter extends FragmentPagerAdapter{
+
+        public MyAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        /**
+         * Return fragment with respect to Position .
+         */
+
+        @Override
+        public Fragment getItem(int position)
+        {
+            switch (position){
+                case 0 : return new FavouritesFragment();
+                case 1 : return new EventsFragment();
+                case 2 : return new ThisWeekFragment();
+                case 3 : return new NotificationsFragment();
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+
+            return int_items;
+
+        }
+
+        /**
+         * This method returns the title of the tab according to the position.
+         */
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+
+            switch (position){
+                case 0 :
+                    return "FAVOURITES";
+                case 1 :
+                    return "EVENTS";
+                case 2 :
+                    return "THIS WEEK";
+                case 3:
+                    return "NOTIFICATIONS";
+            }
+            return null;
+        }
+    }
+
+}
